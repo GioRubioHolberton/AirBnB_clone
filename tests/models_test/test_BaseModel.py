64ad5910ef334d5a81b456fc8ba9f5a4fef9381e
GioRubioHolberton <759@holbertonschool.com>
2019-07-03 20:33:46 +0000

    1st commit test

---
+++ tests/models_test/test_BaseModel.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+BaseModel unittests
+"""
+import unittest
+from models.base_model import BaseModel
+
+
+class BaseModel_test(unittest.TestCase):
+    """ testing BaseModel """
+
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
