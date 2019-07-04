1bfb34ecdc0f97882ba1ae4513286b00461ca71f
GioRubioHolberton <759@holbertonschool.com>
2019-07-04 01:07:50 +0000

    BaseModel Test

--- tests/models_test/test_BaseModel.py
+++ tests/models_test/test_BaseModel.py
@@ -9,12 +9,23 @@ from models.base_model import BaseModel
 class BaseModel_test(unittest.TestCase):
     """ testing BaseModel """
 
-
     def setUp(self):
-        pass
+        """ New BaseModel  """
+        self.new_model = BaseModel()
 
     def tearDown(self):
-        pass
+        """ Delete  """
+        del cls.insta
+        try:
+            os.remove('file.json')
+        except BaseException:
+            pass
+
+    def test_pep8(self):
+            """ pep8 testing """
+            p8 = pep8.StyleGuide(quiet=True)
+            asw = pep_8.check_files(['models/base_model.py'])
+            self.assertEqual(answe.total_errors, 0, "Fix Style")
 
 if __name__ == "__main__":
     unittest.main()
