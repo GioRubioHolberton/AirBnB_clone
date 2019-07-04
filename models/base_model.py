df4f1b3d7e83868fb33cdccd6e960d2abbe37b1d
GaviriaAmaya <judamayaga@unal.edu.co>
2019-07-04 04:46:45 +0000

    solution for exercise 4

--- models/base_model.py
+++ models/base_model.py
@@ -14,14 +14,13 @@ class BaseModel:
         COntains the basics to the future classes'
 
         if (kwargs):
-            for keys, values in self.__dict__.items():
-                if keys != '__class__':
+            for keys, values in kwargs.items():
+                if keys != "__class__":
                     if keys == "created_at" or keys == "updated_at":
                         valt = datetime.strptime(values,
                                                  "%Y-%m-%dT%H:%M:%S.%f")
                         setattr(self, keys, valt)
                     else:
-                        setattr(self, keys, values)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -35,12 +34,13 @@ class BaseModel:
     def save(self):
         'Saves date and time for each update'
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
         'Converts the object on a dictionary'
         ob_to_dict = self.__dict__.copy()
-        ob_to_dict['created_at'] = self.created_at.isoformat()
-        ob_to_dict['updated_at'] = self.updated_at.isoformat()
-        ob_to_dict['__class__'] = self.__class__.__name__
+        ob_to_dict["__class__"] = self.__class__.__name__
+        ob_to_dict["created_at"] = self.created_at.isoformat()
+        ob_to_dict["updated_at"] = self.updated_at.isoformat()
         return ob_to_dict
